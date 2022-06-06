05c9e96e3d1701ba692009ce631dff0592733b84
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-06-06 18:52:33 +0800

    feat: update vue version

--- src/components/NumberSubmit.vue
+++ src/components/NumberSubmit.vue
@@ -1,28 +1,25 @@
+<!--
+ * @Author: Evan Zuo v_wangxiangbo01@baidu.com
+ * @Date: 2022-06-06 14:59:00
+ * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
+ * @LastEditTime: 2022-06-06 18:49:24
+ * @FilePath: /my-first-taro-app/src/components/NumberSubmit.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <input v-model="number" type="number" class="input" />
-  <button @tap="addNumber">
-    Add new number
-  </button>
+  <button @tap="addNumber">Add new number</button>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
 import { useStore } from 'vuex'
 
-export default {
-  setup () {
-    const store = useStore()
-    const number = ref(0)
+const store = useStore()
+const number = ref(0)
 
-    function addNumber() {
-      store.dispatch('addNumber', Number(number.value))
-    }
-
-    return {
-      number,
-      addNumber
-    }
-  }
+function addNumber() {
+  store.dispatch('addNumber', Number(number.value))
 }
 </script>
 
