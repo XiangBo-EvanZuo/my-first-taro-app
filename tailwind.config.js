86449a828c15f66962013909b5d926eff1179455
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-06-07 13:24:38 +0800

    feat: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./src/**/*.js",
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+    "./src/**/*.wxml",
+  ],
+  corePlugins: {},
+  variants: {},
+  theme: {},
+};
