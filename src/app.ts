4ae6874c4cf8234d1ca4a05b07937eee7598d3a7
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-06-08 15:03:47 +0800

    feat: persist

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
  * @Author: Evan Zuo v_wangxiangbo01@baidu.com
  * @Date: 2022-06-07 16:45:13
  * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
- * @LastEditTime: 2022-06-08 14:09:09
+ * @LastEditTime: 2022-06-08 15:00:06
  * @FilePath: /my-first-taro-app/src/app.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,9 +17,9 @@ const App = createApp({
     onShow(options) { },
     // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
-
+const pinaInstance = createPinia()
 App
-    .use(createPinia())
+  .use(pinaInstance)
     .use(Button)
     .use(Toast)
     .use(Switch)
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,30 +2,34 @@
  * @Author: Evan Zuo v_wangxiangbo01@baidu.com
  * @Date: 2022-06-08 13:50:27
  * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
- * @LastEditTime: 2022-06-08 14:20:56
+ * @LastEditTime: 2022-06-08 15:02:10
  * @FilePath: /my-first-taro-app/src/store/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // https://pinia.esm.dev/introduction.html
+import Taro from '@tarojs/taro';
 import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore('counter', {
-    state: () => {
-        return {
-          count: 0,
-          isDarkMode: false,
-        };
+  state: () => {
+    return {
+      count: Taro.getStorageSync('count') || 0,
+      isDarkMode: Taro.getStorageSync('isDarkMode') || false,
+    };
+  },
+
+  // could also be defined as
+  // state: () => ({ count: 0 })
+  actions: {
+    increment() {
+      this.count++;
+      Taro.setStorageSync('count', this.count)
     },
-    // could also be defined as
-    // state: () => ({ count: 0 })
-    actions: {
-        increment() {
-            this.count++;
-        },
-        triggerDarkMode() {
-            this.isDarkMode = !this.isDarkMode
-        }
-    },
+    triggerDarkMode() {
+      this.isDarkMode = !this.isDarkMode
+      Taro.setStorageSync('isDarkMode', this.isDarkMode)
+    }
+  },
 });
 
 // You can even use a function (similar to a component setup()) to define a Store for more advanced use cases:
